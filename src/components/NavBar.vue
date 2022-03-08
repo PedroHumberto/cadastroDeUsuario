acc2fd02317f40db6f006ea9b48360ce10311480
Pedro Humberto <pedrohumbertogc@gmail.com>
2022-03-07 22:27:43 -0300

    navbar, vuex, removido localstorage, refatorando requisição http

---
+++ src/components/NavBar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-bytebank">
+      <a class="navbar-brand" href="#">ByteBank</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <NavBarLoged v-if="usuarioLogado"/>
+        <NavBarLogout v-else/>      
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+import NavBarLoged from "./NavBarLoged";
+import NavBarLogout from "./NavBarLogout";
+export default {
+  components: {
+    NavBarLoged,
+    NavBarLogout,
+  },
+  computed: {
+    usuarioLogado() {
+      return Boolean(this.$store.state.token);
+    },
+  },
+};
+</script>
+
+<style>
+.navbar {
+  background: #27ae60;
+}
+.navbar-bytebank a {
+  color: #fff;
+}
+.navbar-bytebank a:hover {
+  color: #000;
+}
+</style>
---
+++ src/provedor.js
@@ -0,0 +1,48 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import http from './http'
+
+Vue.use(Vuex)
+
+const estado = {
+    token: null,
+    usuario: {}
+}
+
+
+const mutations= {
+    DEFINIR_USUARIO_LOGADO(state, { token, usuario }){
+        state.usuario = usuario
+        state.token = token
+    },
+    DESLOGAR_USUARIO(state){
+        state.token = null
+        state.usuario= {}
+    }
+}
+
+const actions = {
+    efetuarLogin({ commit }, usuario){//destructuring assingment
+        return new Promise( (resolve, reject) => {
+            http.post('auth/login', usuario)
+                .then(res =>{
+                    commit('DEFINIR_USUARIO_LOGADO', {
+                        token: res.data.access_token,
+                        usuario: res.data.user
+                    })
+                    resolve(res.data)
+                })
+                .catch(err => {
+                    console.log(err)
+                    reject(err)
+                })
+        })
+
+    }
+}
+
+export default new Vuex.Store({
+    state:estado,
+    mutations,
+    actions
+})
